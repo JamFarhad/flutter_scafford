1faff1eb17eaa8857ca0adfed78e40c1f0f72fc9
Farhad Athar <jamfarhadathar@gmail.com>
2024-09-24 17:08:29 +0500

    Adaptiveness

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_responsiveness/landscape_login.dart';
-
 import 'package:flutter_responsiveness/portriat_login.dart';
 
 void main() {
